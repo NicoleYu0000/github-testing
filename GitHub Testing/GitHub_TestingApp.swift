57bf79f843b28614aa3cda2a7b6206406cfaf347
NicoleYu0000 <littleco6688@gmail.com>
2023-05-31 11:08:24 +0800

    Initial Commit

---
+++ GitHub Testing/GitHub_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestingApp.swift
+//  GitHub Testing
+//
+//  Created by Nicole Yu on 2023/5/31.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
